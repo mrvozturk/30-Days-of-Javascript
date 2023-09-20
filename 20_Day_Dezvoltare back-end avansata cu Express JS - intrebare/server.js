b10b2c893171d65b07239bffe0ec00593182185b
Merve Ozturk <ozturkmerve745@gmail.com>
2023-09-20 18:04:39 +0300

    config eklendi

--- 20_Day_Dezvoltare back-end avansata cu Express JS - intrebare/server.js
+++ 20_Day_Dezvoltare back-end avansata cu Express JS - intrebare/server.js
@@ -1,12 +1,18 @@
 const express = require("express");
+const dotenv = require("./config/env/node_modules/dotenv/lib/main");
+
+// Environment Variables
+dotenv.config({
+    path: "./config/env/config.env"
+});
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
-const PORT = 5000 || process.env.Port;
-
-app.get("/",(req,res)=>{
-    res.send("Hello Question Answer Api Updated")
+app.get("/", (req, res) => {
+    res.send("Hello Question Answer Api Updated");
 });
+
 app.listen(PORT, () => {
-    console.log(`App Started on ${PORT}`);
-})
+    console.log(`App Started on ${PORT} : ${process.env.NODE_ENV}`);
+});
